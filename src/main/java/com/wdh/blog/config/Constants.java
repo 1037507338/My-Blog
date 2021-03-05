fce74b9e5d3e3e98069b94fd7b27ec6c5484c99d
wangdonghao <wangdonghao@vipkid.com.cn>
2021-03-05 16:00:24 +0800

    update

--- src/main/java/com/wdh/blog/config/Constants.java
+++ src/main/java/com/wdh/blog/config/Constants.java
@@ -4,7 +4,7 @@ package com.wdh.blog.config;
  * @author wangdonghao
  */
 public class Constants {
-    //上传文件的默认url前缀，根据部署设置自行修改
+
     public final static String FILE_UPLOAD_DIC = "/upload/";
 
     public final static String BACK_DOOR_KEY = "54188wang";
--- src/main/java/com/wdh/blog/config/MyBlogWebMvcConfigurer.java
+++ src/main/java/com/wdh/blog/config/MyBlogWebMvcConfigurer.java
@@ -1,11 +1,12 @@
 package com.wdh.blog.config;
 
 import com.wdh.blog.interceptor.AdminLoginInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
 
 /**
  * @author wangdonghao
@@ -13,14 +14,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyBlogWebMvcConfigurer implements WebMvcConfigurer {
 
-    @Autowired
+    @Resource
     private AdminLoginInterceptor adminLoginInterceptor;
 
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 添加一个拦截器，拦截以/admin为前缀的url路径
         registry.addInterceptor(adminLoginInterceptor).addPathPatterns("/admin/**").excludePathPatterns("/admin/login").excludePathPatterns("/admin/dist/**").excludePathPatterns("/admin/plugins/**");
     }
 
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**").addResourceLocations("file:" + Constants.FILE_UPLOAD_DIC);
     }
