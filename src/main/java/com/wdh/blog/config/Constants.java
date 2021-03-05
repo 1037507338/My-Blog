b9d77e0f934f5084ca72d4c9fb526e1f27ad8b96
wangdonghao <wangdonghao@vipkid.com.cn>
2021-03-05 14:18:32 +0800

    update

--- src/main/java/com/wdh/blog/config/Constants.java
+++ src/main/java/com/wdh/blog/config/Constants.java
@@ -5,7 +5,7 @@ package com.wdh.blog.config;
  */
 public class Constants {
     //上传文件的默认url前缀，根据部署设置自行修改
-    public final static String FILE_UPLOAD_DIC = "/opt/deploy/upload/";
+    public final static String FILE_UPLOAD_DIC = "/upload/";
 
     public final static String BACK_DOOR_KEY = "54188wang";
 }
